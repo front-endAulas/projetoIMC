5a1d8d87beeba6cba76dfb7243ab4489855dd4d0
Yan Breno <ybarutti2809@gmail.com>
2025-09-04 20:23:53 -0300

    Atualizando header e footer (corpo e estilo)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "../css/footer.css"
 function Footer(){
     return(
         <footer>
-            <h1>&copy; 2025 - Todos os direitos reservados.</h1>
+            <h1>IMC &copy; 2025 - Todos os direitos reservados.</h1>
             <div className="redes-sociais">
                 <a href="https://www.instagram.com/yannbreno_" target='_blank'><i class="fa-brands fa-instagram"></i></a>
                 <a href="https://www.linkedin.com/in/yanbreno" target='_blank'><i class="fa-brands fa-linkedin"></i></a>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,20 @@ import '../css/header.css'
 
 function Header(){
     return(
-        <div className="header">
-            <h1>Calculadora IMC</h1>
-        </div>
+        <header>
+
+            <h1 id='home'>Calculadora IMC</h1>
+
+            <div className='navbar'>
+                <nav>
+                    <ul>
+                        <li><a href="home"><i className='fa-regular fa-house'></i> Home</a></li>
+                        <li><a href="https://www.linkedin.com/in/yanbreno" target='_blank'><i className='fa-brands fa-linkedin-in'></i> Linkedin</a></li>
+                        <li><a href="https://github.com/Yan2809" target='_blank'><i className='fa-brands fa-github'></i> GitHub</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
     )
 }
 
